135b0d7bd9904690b02311009f73eaaeb5b99462
Kalhar Pandya <kalhar.p@asambhav.in>
2021-03-30 01:20:10 +0530

    added new compute

--- motorPair.h
+++ motorPair.h
@@ -10,22 +10,20 @@ class motorPair{
     bool brake = false;
     bool enable = false;
     bool stable = false;
-    
-    PID *myPID =  new PID(&Input,&Output,&Setpoint,Kp,Ki,Kd,DIRECT);
     double Kp=0, Ki=0, Kd = 0;
     double Setpoint = 0,Output = 0,Input = 0;
+    PID *myPID =  new PID(&Input,&Output,&Setpoint,Kp,Ki,Kd,DIRECT);
     Motor *m1,*m2;
     int PWM = 0;
     
-
     void getReadings();
-    // void SetReadings(long readings);
+    void setReadings(long readings);
     void reset();
     void setTunnings(double Kp , double Ki ,double Kd);
     void setPWM(int pwm);
     void compute();
-    void brakeEnable(bool brake);
-    void brakeDisable(bool brake);
+    void brakeEnable(bool brake = true);
+    void brakeDisable(bool brake = true);
 };
 
 #endif
